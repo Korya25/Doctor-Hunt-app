026747909a8745a6e783a5d8c955fd399b04fc13
Korya25 <enthrj3@gmail.com>
2025-06-08 13:08:11 +0300

    add intial core file

---
+++ lib/core/constant/app_assets.dart
@@ -0,0 +1,28 @@
+class AppImages {
+  static const List<String> onboarding = [
+    'assets/images/onboarding screen-01.png',
+    'assets/images/onboarding screen-02.png',
+    'assets/images/onboarding screen-03.png',
+  ];
+
+  static const List<String> onboardingBackground = [
+    'assets/images/Onboarding background left.png',
+    'assets/images/onboarding background right.png',
+  ];
+  static const String backgroundScaffold =
+      'assets/images/background_scaffold.png';
+}
+
+class AppIconsSvgs {
+  static const String appIcon = 'assets/icons/app_icon.svg';
+  static const String arrowIos = 'assets/icons/arrow_ios.svg';
+  static const String star = 'assets/icons/star.svg';
+  static const String favorite = 'assets/icons/favorite.svg';
+  static const String nonFavorite = 'assets/icons/non_favorite.svg';
+  static const String sprocket = 'assets/icons/sprocket.svg';
+  static const String heart = 'assets/icons/heart.svg';
+  static const String eye = 'assets/icons/eye.svg';
+  static const String body = 'assets/icons/body.svg';
+  static const String live = 'assets/icons/live_icon.svg';
+  static const String playVedio = 'assets/icons/play_viedio.svg';
+}
---
+++ lib/core/resources/app_text_styles.dart
@@ -0,0 +1,77 @@
+import 'package:doctor_hunt/core/constant/app_colors.dart';
+import 'package:doctor_hunt/core/utils/enums.dart';
+import 'package:doctor_hunt/core/utils/extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static TextStyle _rubik({
+    required double fontSize,
+    required FontWeightType weight,
+    double letterSpacing = 0,
+    required Color color,
+  }) {
+    return GoogleFonts.rubik(
+      fontSize: fontSize.sp,
+      fontWeight: weight.fontWeight,
+      letterSpacing: letterSpacing,
+      color: color,
+    );
+  }
+
+  // Onboarding Screen
+  static TextStyle get rubik28MediumPrimary => _rubik(
+    fontSize: 28.sp,
+    weight: FontWeightType.medium,
+    letterSpacing: -0.3,
+    color: AppColors.primaryTextColor,
+  );
+
+  static TextStyle get rubik14RegularTiary => _rubik(
+    fontSize: 14.sp,
+    weight: FontWeightType.regular,
+    letterSpacing: -0.3,
+    color: AppColors.tertiaryTextColor,
+  );
+
+  // Buttons
+  static TextStyle get rubik18MediumSecondary => _rubik(
+    fontSize: 18.sp,
+    weight: FontWeightType.medium,
+    color: AppColors.secondaryTextColor,
+  );
+
+  static TextStyle get rubik14MediumTertiary => _rubik(
+    fontSize: 14.sp,
+    weight: FontWeightType.medium,
+    letterSpacing: -0.3,
+    color: AppColors.tertiaryTextColor,
+  );
+
+  static TextStyle get rubikLight20Secondary => _rubik(
+    fontSize: 20.sp,
+    weight: FontWeightType.light,
+    letterSpacing: -0.3,
+    color: AppColors.secondaryTextColor,
+  );
+  static TextStyle get rubikBold25Secondary => _rubik(
+    fontSize: 25.sp,
+    weight: FontWeightType.bold,
+    letterSpacing: -0.3,
+    color: AppColors.secondaryTextColor,
+  );
+
+  static TextStyle get rubik18MediumPrimary => _rubik(
+    fontSize: 18.sp,
+    weight: FontWeightType.medium,
+    letterSpacing: -0.3,
+    color: AppColors.primaryTextColor,
+  );
+  static TextStyle get rubik12LightTertiary => _rubik(
+    fontSize: 12.sp,
+    weight: FontWeightType.light,
+    letterSpacing: -0.3,
+    color: AppColors.tertiaryTextColor,
+  );
+}
